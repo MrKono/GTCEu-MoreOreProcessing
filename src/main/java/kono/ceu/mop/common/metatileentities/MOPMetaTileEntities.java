ec7ce02be9f6b77a0d521da8e24d2430c797c027
ko_no <90126004+MrKono@users.noreply.github.com>
2025-02-22 22:01:43 +0900

    add Industrial Ore Factory

--- src/main/java/kono/ceu/mop/common/metatileentities/MOPMetaTileEntities.java
+++ src/main/java/kono/ceu/mop/common/metatileentities/MOPMetaTileEntities.java
@@ -4,6 +4,7 @@ import static gregtech.common.metatileentities.MetaTileEntities.registerMetaTile
 import static kono.ceu.mop.api.MOPValues.mopId;
 
 import kono.ceu.mop.MOPConfig;
+import kono.ceu.mop.common.metatileentities.multi.electric.MetaTileEntityIndustrialOreFactory;
 import kono.ceu.mop.common.metatileentities.multi.electric.MetaTileEntityOreFactory;
 import kono.ceu.mop.common.metatileentities.multi.primitive.MetaTileEntityBronzePlatedBlastFurnace;
 import kono.ceu.mop.common.metatileentities.multi.primitive.MetaTileEntityBronzeReinforcedBlastFurnace;
@@ -14,6 +15,7 @@ public class MOPMetaTileEntities {
     public static MetaTileEntityBronzePlatedBlastFurnace BRONZE_PLATED_BLASE_FURNACE;
     public static MetaTileEntityBronzeReinforcedBlastFurnace BRONZE_REINFORCED_BLAST_FURNACE;
     public static MetaTileEntityOreFactory ORE_FACTORY;
+    public static MetaTileEntityIndustrialOreFactory INDUSTRIAL_ORE_FACTORY;
 
     public static void init() {
         // Multi Machine
@@ -25,5 +27,7 @@ public class MOPMetaTileEntities {
                 new MetaTileEntityBronzeReinforcedBlastFurnace(mopId("bronze_reinforced_blast_furnace")));
         ORE_FACTORY = registerMetaTileEntity(++id,
                 new MetaTileEntityOreFactory(mopId("ore_factory")));
+        INDUSTRIAL_ORE_FACTORY = registerMetaTileEntity(++id,
+                new MetaTileEntityIndustrialOreFactory(mopId("industrial_ore_factory")));
     }
 }
